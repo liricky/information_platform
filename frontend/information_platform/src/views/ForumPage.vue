165c42baddc3661e56b4e280a0c2096c1e6f764a
liricky <15901912427@163.com>
2019-01-24 14:40:21 +0800

    0.06

--- frontend/information_platform/src/views/ForumPage.vue
+++ frontend/information_platform/src/views/ForumPage.vue
@@ -79,7 +79,31 @@
               </Col>
             </Row>
           </TabPane>
+          <TabPane label="全部">
+            <Row class="cardbox" style="background:#eee;padding:20px">
+              <Col class="cardcol" span="25" v-for="(post3,index) in post3" :key="post3.title">
+                <div @click="jumpDetail(post3.id)">
+                  <Card class="card" :bordered="true">
+                    <h3 class="headline" slot="title">{{post3.label}} {{post3.author}}</h3>
+                    <h1>{{post3.title}}</h1>
+                    <p>{{post3.date}}</p>
+                    <div class="rightback">
+                      <div>
+                        <Icon type="ios-heart" size="19"/>
+                        <font size="4">{{post3.likenum}}</font>
+                      </div>
+                      <div>
+                        <Icon type="ios-chatbubbles" size="19"/>
+                        <font size="4">{{post3.commentnum}}</font>
+                      </div>
+                    </div>
+                  </Card>
+                </div>
+              </Col>
+            </Row>
+          </TabPane>
         </Tabs>
+        <Button class="sendbutton" type="primary" shape="circle" icon="ios-create" size="large"><font size="3">发帖</font></Button>
       </div>
     </div>
     <bottom></bottom>
@@ -105,6 +129,10 @@
     position: relative;
     bottom: 110px;
     left: 47%;
+  }
+  .sendbutton{
+   position: relative;
+    left:45%;
   }
 </style>
 <script>
@@ -187,6 +215,30 @@
                   readnum: 20
                 }
               ],
+              post3:[
+                {
+                  id: 6,
+                  label: '体育3',
+                  title: '新生杯3',
+                  author: '16122075',
+                  content: '新生杯比赛即将开始，欢迎加入3！',
+                  date: '2019-01-01',
+                  commentnum: 20,
+                  likenum: 40,
+                  readnum: 50
+                },
+                {
+                  id: 7,
+                  label: '体育3',
+                  title: '校园运动会3',
+                  author: '16122076',
+                  content: '校园运动会即将在本周召开，欢迎同学参加3！',
+                  date: '2019-01-01',
+                  commentnum: 5,
+                  likenum: 0,
+                  readnum: 20
+                }
+              ]
             }
         },
         components: {
