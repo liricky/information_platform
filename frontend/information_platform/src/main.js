aa93fc1550e9b6d612be6c1f6f0543225ebde0f8
liricky <15901912427@163.com>
2019-01-27 22:52:58 +0800

    0.09

--- frontend/information_platform/src/main.js
+++ frontend/information_platform/src/main.js
@@ -5,9 +5,12 @@ import App from './App'
 import router from './router'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 Vue.use(iView)
+Vue.use(VueAxios,axios)
 
 /* eslint-disable no-new */
 new Vue({
