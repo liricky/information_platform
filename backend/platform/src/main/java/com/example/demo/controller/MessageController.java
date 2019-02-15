db82c4dd5df45f2aab270358ea3f8e16813c1ecc
liricky <15901912427@163.com>
2019-02-15 10:10:48 +0800

    私信部分controller层完成

--- backend/platform/src/main/java/com/example/demo/controller/MessageController.java
+++ backend/platform/src/main/java/com/example/demo/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.UsersMapper;
 import com.example.demo.model.entity.Users;
+import com.example.demo.model.jsonRequest.MessageSend;
 import com.example.demo.model.ov.Result;
 import com.example.demo.service.MessageService;
 import com.example.demo.tools.ResultTool;
@@ -23,15 +24,43 @@ public class MessageController {
 
     @PostMapping("/receive/{userid}")
     public Result messageReceive(@RequestHeader(value = "Authorization") String token, @PathVariable("userid") String userid){
-        System.out.println(token);
-        System.out.println(userid);
         if(StringUtils.isEmpty(token) || StringUtils.isEmpty(userid)){
-            return ResultTool.error("传入数据为空！");
+            return ResultTool.error("传入数据不能空！");
         }
-
         Users users = usersMapper.getById(userid);
         if(users.getToken().equals(token)){
             return messageService.messagereceive(userid);
+        } else{
+            return ResultTool.error("登录状态验证失败！");
+        }
+    }
+
+    @PostMapping("/sent/{userid}")
+    public Result messageSent(@RequestHeader(value = "Authorization") String token, @PathVariable("userid") String userid){
+        if(StringUtils.isEmpty(token) || StringUtils.isEmpty(userid)){
+            return ResultTool.error("传入数据不能空！");
+        }
+        Users users = usersMapper.getById(userid);
+        if(users.getToken().equals(token)){
+            return messageService.messagesent(userid);
+        } else{
+            return ResultTool.error("登录状态验证失败！");
+        }
+    }
+
+    @PostMapping("/send/{userid}/{sendid}/{title}/{content}")
+    public Result messageSend(@RequestHeader(value = "Authorization") String token, @PathVariable("userid") String userid, @PathVariable("sendid") String sendid, @PathVariable("title") String title, @PathVariable("content") String content){
+        if(StringUtils.isEmpty(token) || StringUtils.isEmpty(userid) || StringUtils.isEmpty(sendid) || StringUtils.isEmpty(title) || StringUtils.isEmpty(content)){
+            return ResultTool.error("传入数据不能空！");
+        }
+        Users users = usersMapper.getById(userid);
+        if(users.getToken().equals(token)){
+            MessageSend messageSend = new MessageSend();
+            messageSend.setUserid(userid);
+            messageSend.setSendid(sendid);
+            messageSend.setTitle(title);
+            messageSend.setContent(content);
+            return messageService.messagesend(messageSend);
         } else{
             return ResultTool.error("登录状态验证失败！");
         }
