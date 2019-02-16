ab1493ae286165bea236789e56628013ec51f8f8
Tuangtuang <tangyuqi00@qq.com>
2019-02-16 15:29:00 +0800

    修正了数据库

--- backend/platform/src/main/java/com/example/demo/dao/ManagersMapper.java
+++ backend/platform/src/main/java/com/example/demo/dao/ManagersMapper.java
@@ -10,21 +10,13 @@ public interface ManagersMapper {
 
     int deleteByExample(ManagersExample example);
 
-    int deleteByPrimaryKey(Integer id);
-
     int insert(Managers record);
 
     int insertSelective(Managers record);
 
     List<Managers> selectByExample(ManagersExample example);
 
-    Managers selectByPrimaryKey(Integer id);
-
     int updateByExampleSelective(@Param("record") Managers record, @Param("example") ManagersExample example);
 
     int updateByExample(@Param("record") Managers record, @Param("example") ManagersExample example);
-
-    int updateByPrimaryKeySelective(Managers record);
-
-    int updateByPrimaryKey(Managers record);
 }
--- backend/platform/src/main/java/com/example/demo/model/entity/Managers.java
+++ backend/platform/src/main/java/com/example/demo/model/entity/Managers.java
@@ -1,16 +1,16 @@
 package com.example.demo.model.entity;
 
 public class Managers {
-    private Integer id;
+    private String id;
 
     private String password;
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
     }
 
     public String getPassword() {
