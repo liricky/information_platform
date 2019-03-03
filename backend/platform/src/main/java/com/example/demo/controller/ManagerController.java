124d74e565504de6c5579115876e1951237275b7
Tuangtuang <tangyuqi00@qq.com>
2019-03-03 14:09:30 +0800

    管理员controller层完成

--- backend/platform/src/main/java/com/example/demo/controller/ManagerController.java
+++ backend/platform/src/main/java/com/example/demo/controller/ManagerController.java
@@ -1,15 +1,138 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.jsonRequest.deleteViewByManager;
+import com.example.demo.model.jsonRequest.freeUserByManager;
+import com.example.demo.model.jsonRequest.modifyPasswordByManager;
+import com.example.demo.model.ov.Result;
 import com.example.demo.service.ManagerService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.tools.JwtUtil;
+import com.example.demo.tools.ResultTool;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @CrossOrigin
 public class ManagerController {
     @Resource
     private ManagerService managerService;
-    
+
+    //  管理员获取用户信息接口 #49
+    @GetMapping(value = "/manage/user/{manageid}")
+    public Result getUsersInfo(HttpServletRequest httpServletRequest,
+                               @PathVariable("manageid") String managerId){
+        String token = httpServletRequest.getHeader("Authorization");
+        String id;
+        try {
+            id= JwtUtil.parseJwt(token);
+        }catch (Exception e){
+            return ResultTool.error("登陆状态无效");
+        }
+        if (!managerId.equals(id)){
+            return ResultTool.error("登陆状态无效");
+        }
+        return managerService.getUserInfo(managerId);
+    }
+
+    //  管理员获取申诉信息接口 #56
+    @GetMapping(value = "/manage/inbox/appeal/{manageid}")
+    public Result getApplyInfo(HttpServletRequest httpServletRequest,
+                               @PathVariable("manageid") String managerId){
+        String token = httpServletRequest.getHeader("Authorization");
+        String id;
+        try {
+            id= JwtUtil.parseJwt(token);
+        }catch (Exception e){
+            return ResultTool.error("登陆状态无效");
+        }
+        if (!managerId.equals(id)){
+            return ResultTool.error("登陆状态无效");
+        }
+        return managerService.getApplyInfo(managerId);
+    }
+
+    //  管理员获取举报信息接口 #55
+    @GetMapping(value = "/manage/inbox/report/{manageid}")
+    public Result getAlarmInfo(HttpServletRequest httpServletRequest,
+                               @PathVariable("manageid") String managerId){
+        String token = httpServletRequest.getHeader("Authorization");
+        String id;
+        try {
+            id= JwtUtil.parseJwt(token);
+        }catch (Exception e){
+            return ResultTool.error("登陆状态无效");
+        }
+        if (!managerId.equals(id)){
+            return ResultTool.error("登陆状态无效");
+        }
+        return managerService.getAlarmInfo(managerId);
+    }
+
+    //  管理员获取帖子信息接口 #53
+    @GetMapping(value = "/manage/forum/{manageid}")
+    public Result getViewInfo(HttpServletRequest httpServletRequest,
+                              @PathVariable("managerid")String managerId){
+        String token = httpServletRequest.getHeader("Authorization");
+        String id;
+        try {
+            id= JwtUtil.parseJwt(token);
+        }catch (Exception e){
+            return ResultTool.error("登陆状态无效");
+        }
+        if (!managerId.equals(id)){
+            return ResultTool.error("登陆状态无效");
+        }
+        return managerService.getAllViews(managerId);
+    }
+
+    //  管理员修改用户密码 #50
+    @RequestMapping(value = "/manage/user/password",method = RequestMethod.POST)
+    public Result modifyPassword(HttpServletRequest httpServletRequest,
+                                 @RequestBody modifyPasswordByManager modifyPasswordByManager){
+        String token = httpServletRequest.getHeader("Authorization");
+        String id;
+        try {
+            id= JwtUtil.parseJwt(token);
+        }catch (Exception e){
+            return ResultTool.error("登陆状态无效");
+        }
+        if (!modifyPasswordByManager.getManagerId().equals(id)){
+            return ResultTool.error("登陆状态无效");
+        }
+        return managerService.modifyPassword(modifyPasswordByManager);
+    }
+
+    //  管理员删除帖子接口 #54
+    @RequestMapping(value = "/manage/forum/delete",method = RequestMethod.POST)
+    public  Result deleteView(HttpServletRequest httpServletRequest, deleteViewByManager deleteViewByManager){
+        String token = httpServletRequest.getHeader("Authorization");
+        String id;
+        try {
+            id= JwtUtil.parseJwt(token);
+        }catch (Exception e){
+            return ResultTool.error("登陆状态无效");
+        }
+        if (!deleteViewByManager.getManagerId().equals(id)){
+            return ResultTool.error("登陆状态无效");
+        }
+        return managerService.deleteViewByManager(deleteViewByManager);
+    }
+
+    //  管理员解封用户接口 #52
+    @RequestMapping(value = "/manage/user/release",method = RequestMethod.POST)
+    public Result freeUser(HttpServletRequest httpServletRequest, freeUserByManager freeUserByManager){
+        String token = httpServletRequest.getHeader("Authorization");
+        String id;
+        try {
+            id= JwtUtil.parseJwt(token);
+        }catch (Exception e){
+            return ResultTool.error("登陆状态无效");
+        }
+        if (!freeUserByManager.getManagerId().equals(id)){
+            return ResultTool.error("登陆状态无效");
+        }
+        return managerService.freeUsers(freeUserByManager);
+
+    }
 }
